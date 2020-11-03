d05ec45bede05eadfb2adf990e16387edf723c1a
xanthics <3137623+xanthics@users.noreply.github.com>
2020-11-03 11:57:19 -0800

    table of where stuff drops

---
+++ farming_data.py
@@ -0,0 +1,350 @@
+farming_data = {
+	'dvalins_claw': {
+		'when': ['any'],
+		'where': ['stormterror']
+	},
+	'dvalins_plume': {
+		'when': ['any'],
+		'where': ['stormterror']
+	},
+	'dvalins_sigh': {
+		'when': ['any'],
+		'where': ['stormterror']
+	},
+	'ring_of_boreas': {
+		'when': ['any'],
+		'where': ['wolf_of_the_north']
+	},
+	'spirit_locket_of_boreas': {
+		'when': ['any'],
+		'where': ['wolf_of_the_north']
+	},
+	'tail_of_boreas': {
+		'when': ['any'],
+		'where': ['wolf_of_the_north']
+	},
+	'agnidus_agate': {
+		'when': ['any'],
+		'where': ['pyro_regisvine', 'wolf_of_the_north']
+	},
+	'brilliant_diamond': {
+		'when': ['any'],
+		'where': ['adventure_rank']
+	},
+	'prithiva_topaz': {
+		'when': ['any'],
+		'where': ['geo_hypostasis', 'wolf_of_the_north']
+	},
+	'shivada_jade': {
+		'when': ['any'],
+		'where': ['cryo_regisvine', 'wolf_of_the_north']
+	},
+	'vajrada_amethyst': {
+		'when': ['any'],
+		'where': ['electro_hypostasis', 'stormterror']
+	},
+	'varunada_lazurite': {
+		'when': ['any'],
+		'where': ['oceanid', 'stormterror']
+	},
+	'vayuda_turqoise': {
+		'when': ['any'],
+		'where': ['anemo_hypostasis', 'stormterror']
+	},
+	'basalt_pillar': {
+		'when': ['any'],
+		'where': ['geo_hypostasis']
+	},
+	'cleansing_heart': {
+		'when': ['any'],
+		'where': ['oceanid']
+	},
+	'everflame_seed': {
+		'when': ['any'],
+		'where': ['pyro_regisvine']
+	},
+	'hoarfrost_core': {
+		'when': ['any'],
+		'where': ['cryo_regisvine']
+	},
+	'hurricane_seed': {
+		'when': ['any'],
+		'where': ['anemo_hypostasis']
+	},
+	'lightning_prism': {
+		'when': ['any'],
+		'where': ['electro_hypostasis']
+	},
+	'calla_lily': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'cecilia': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'cor_lapis': {
+		'when': ['any'],
+		'where': ['special_liyue']
+	},
+	'dandelion_seed': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'glaze_lilly': {
+		'when': ['any'],
+		'where': ['special_liyue']
+	},
+	'jueyun_chili': {
+		'when': ['any'],
+		'where': ['special_liyue']
+	},
+	'noctilucous_jade': {
+		'when': ['any'],
+		'where': ['special_liyue']
+	},
+	'philanemo_mushroom': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'silk_flower': {
+		'when': ['any'],
+		'where': ['special_liyue']
+	},
+	'small_lamp_grass': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'valberry': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'violetgrass': {
+		'when': ['any'],
+		'where': ['special_liyue']
+	},
+	'windwheel_aster': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'wolfhook': {
+		'when': ['any'],
+		'where': ['special_mondstadt']
+	},
+	'arrowhead': {
+		'when': ['any'],
+		'where': ['ranged_hilichurl']
+	},
+	'f_insignia': {
+		'when': ['any'],
+		'where': ['skirmisher']
+	},
+	'th_insignia': {
+		'when': ['any'],
+		'where': ['treasure_hoarder']
+	},
+	'mask': {
+		'when': ['any'],
+		'where': ['hilichurl']
+	},
+	'nectar': {
+		'when': ['any'],
+		'where': ['whopperflower']
+	},
+	'scroll': {
+		'when': ['any'],
+		'where': ['samachurl']
+	},
+	'slime': {
+		'when': ['any'],
+		'where': ['slimes']
+	},
+	'bone_shard': {
+		'when': ['any'],
+		'where': ['geovishap_hatchling']
+	},
+	'chaos': {
+		'when': ['any'],
+		'where': ['ruin_guard', 'ruin_hunter']
+	},
+	'horn': {
+		'when': ['any'],
+		'where': ['mitachurl']
+	},
+	'ley_line': {
+		'when': ['any'],
+		'where': ['abyss_mage']
+	},
+	'mist': {
+		'when': ['any'],
+		'where': ['electro_cicin_mage']
+	},
+	'sacrificial_knife': {
+		'when': ['any'],
+		'where': ['agent']
+	},
+	'aerosiderite': {
+		'when': ['wed', 'sat', 'sun'],
+		'where': ['hidden_palace_of_lianshan_formula']
+	},
+	'boreal_wolf': {
+		'when': ['tue', 'fri', 'sun'],
+		'where': ['cecilia_garden']
+	},
+	'dandelion_gladiator': {
+		'when': ['wed', 'sat', 'sun'],
+		'where': ['cecilia_garden']
+	},
+	'decarabian': {
+		'when': ['mon', 'thu', 'sun'],
+		'where': ['cecilia_garden']
+	},
+	'guyun': {
+		'when': ['mon', 'thu', 'sun'],
+		'where': ['hidden_palace_of_lianshan_formula']
+	},
+	'mist_veiled_elixer': {
+		'when': ['tue', 'fri', 'sun'],
+		'where': ['hidden_palace_of_lianshan_formula']
+	},
+	'ballad': {
+		'when': ['wed', 'sat', 'sun'],
+		'where': ['forsaken_rift']
+	},
+	'diligence': {
+		'when': ['tue', 'fri', 'sun'],
+		'where': ['taishan_mansion']
+	},
+	'freedom': {
+		'when': ['mon', 'thu', 'sun'],
+		'where': ['forsaken_rift']
+	},
+	'gold': {
+		'when': ['wed', 'sat', 'sun'],
+		'where': ['taishan_mansion']
+	},
+	'prosperity': {
+		'when': ['mon', 'thu', 'sun'],
+		'where': ['taishan_mansion']
+	},
+	'resistance': {
+		'when': ['tue', 'fri', 'sun'],
+		'where': ['forsaken_rift']
+	},
+	'adventurer': {
+		'when': ['any'],
+		'where': ['chests', 'midsummer_courtyard']
+	},
+	'archaic_petra': {
+		'when': ['any'],
+		'where': ['domain_of_guyun']
+	},
+	'berserker': {
+		'when': ['any'],
+		'where': ['chests', 'bosses']
+	},
+	'bloodstained_chivalry': {
+		'when': ['any'],
+		'where': ['clear_pool_and_mountain_cavern']
+	},
+	'brave_heart': {
+		'when': ['any'],
+		'where': ['domain_of_guyun']
+	},
+	'crimson_witch_of_flames': {
+		'when': ['any'],
+		'where': ['hidden_palace_of_zhou_formula']
+	},
+	'defenders_will': {
+		'when': ['any'],
+		'where': ['hidden_palace_of_zhou_formula']
+	},
+	'gambler': {
+		'when': ['any'],
+		'where': ['clear_pool_and_mountain_cavern']
+	},
+	'gladiators_finale': {
+		'when': ['any'],
+		'where': ['stormterror', 'geo_hypostasis', 'oceanid', 'pyro_regisvine', 'cryo_regisvine', 'anemo_hypostasis', 'electro_hypostasis', 'wolf_of_the_north']
+	},
+	'instructor': {
+		'when': ['any'],
+		'where': ['bosses']
+	},
+	'lavawalker': {
+		'when': ['any'],
+		'where': ['hidden_palace_of_zhou_formula']
+	},
+	'lucky_dog': {
+		'when': ['any'],
+		'where': ['chests', 'domain_of_guyun']
+	},
+	'maiden_beloved': {
+		'when': ['any'],
+		'where': ['valley_of_remembrance']
+	},
+	'martial_artist': {
+		'when': ['any'],
+		'where': ['hidden_palace_of_zhou_formula']
+	},
+	'noblesse_oblige': {
+		'when': ['any'],
+		'where': ['clear_pool_and_mountain_cavern']
+	},
+	'prayers_for_destiny': {
+		'when': ['any'],
+		'where': ['oceanid']
+	},
+	'prayers_for_illumination': {
+		'when': ['any'],
+		'where': ['pyro_regisvine']
+	},
+	'prayers_for_wisdom': {
+		'when': ['any'],
+		'where': ['electro_hypostasis']
+	},
+	'prayers_to_springtime': {
+		'when': ['any'],
+		'where': ['cryo_regisvine']
+	},
+	'resolution_of_sojourner': {
+		'when': ['any'],
+		'where': ['midsummer_courtyard']
+	},
+	'retracing_bolide': {
+		'when': ['any'],
+		'where': ['domain_of_guyun']
+	},
+	'scholar': {
+		'when': ['any'],
+		'where': ['clear_pool_and_mountain_cavern']
+	},
+	'the_exile': {
+		'when': ['any'],
+		'where': ['bosses']
+	},
+	'thundering_fury': {
+		'when': ['any'],
+		'where': ['midsummer_courtyard']
+	},
+	'thundersoother': {
+		'when': ['any'],
+		'where': ['midsummer_courtyard']
+	},
+	'tiny_miracle': {
+		'when': ['any'],
+		'where': ['valley_of_remembrance']
+	},
+	'traveling_doctor': {
+		'when': ['any'],
+		'where': ['chests', 'valley_of_remembrance', 'bosses']
+	},
+	'viridescent_venerer': {
+		'when': ['any'],
+		'where': ['valley_of_remembrance']
+	},
+	'wanderers_troupe': {
+		'when': ['any'],
+		'where': ['stormterror', 'geo_hypostasis', 'oceanid', 'pyro_regisvine', 'cryo_regisvine', 'anemo_hypostasis', 'electro_hypostasis', 'wolf_of_the_north']
+	}
+}
